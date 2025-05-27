50c9fd08cfae91bee94e31459cc0f781476d741b
Gite-HOng <ghdrlxo01@naver.com>
2025-05-27 14:29:36 +0900

    진짜 최종

--- my-new-project/my-app/backend/db/connection.js
+++ my-new-project/my-app/backend/db/connection.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 
+// 환경변수에서 DB 연결 정보 불러오기
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "harry2217*",
-  database: "myapp",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "myapp",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
 });
 
+// DB 연결
 db.connect((err) => {
   if (err) {
     console.error("MySQL 연결 실패:", err);
