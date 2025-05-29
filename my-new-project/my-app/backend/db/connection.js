6447119b5137921a0c208b0c82b28f8c2247f733
Gite-HOng <ghdrlxo01@naver.com>
2025-05-29 18:51:49 +0900

    db업로드

--- my-new-project/my-app/backend/db/connection.js
+++ my-new-project/my-app/backend/db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
 
 // 환경변수에서 DB 연결 정보 불러오기
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "myapp",
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
+  host: process.env.DB_HOST,     // trolley.proxy.rlwy.net
+  user: process.env.DB_USER,     // root
+  password: process.env.DB_PASS, // aGrgxhhVfDAtSEuYwmTAaxpmuShztPOd
+  database: process.env.DB_NAME, // railway
+  port: process.env.DB_PORT,     // 28948
 });
 
 // DB 연결
